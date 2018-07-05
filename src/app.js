ddce09cb1492c445145bdfa502c8e61331edd149
Diachenko Oleksandr <odiachenko@exadel.com>
2018-07-05 21:48:14 +0300

    css loader

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import toUpperCase from './mod/utils.js'
+import './app.css'
 var arr = ['a', 'b', 'c'].map(toUpperCase);
 console.log('test')
 document.querySelector('#content').innerHTML = "Cosssast1111"
